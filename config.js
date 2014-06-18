6bdb39013c78d58b18d4df028e1724e114c02b5c
Andrew Zey <andrew@andrewzey.com>
2014-06-17 18:12:58 -0700

    Revert "Corrected missing site variable in config.js"
    This reverts commit 3af384707df042f7014021d351d78e551ebdc528.

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://blog.andrewzey.com',
+        url: 'http://my-ghost-blog.com',
         mail: {},
         database: {
             client: 'sqlite3',
